404d41414153042babe961986c7d41ac1147bf6b
LeoDalcegio <leoodalcegio@hotmail.com>
2020-11-12 15:50:31 -0300

    Added react-ga

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { PageView, initGA, Event } from "../../components/Tracking";
+import ReactGA from "react-ga";
 
 import "./styles.sass";
 
@@ -7,7 +9,14 @@ export default function SearchPage() {
   const [room, setRoom] = useState("");
   const history = useHistory();
 
+  useState(() => {
+    initGA("G-F7Z7PZ0X6L");
+    PageView();
+  }, []);
+
   function handleSubmit() {
+    Event("ROOM", "Room searched", "SEARCH_PAGE");
+
     history.push(room);
   }
 
@@ -17,15 +26,22 @@ export default function SearchPage() {
         <h1>Escrevaldo</h1>
         <p>Escreva qualquer coisa</p>
 
-        <form onSubmit={handleSubmit} netlify>
-          <input
-            type="text"
-            className="room-input"
-            value={room}
-            onChange={(event) => setRoom(event.target.value)}
-            required
-            placeholder="Nome da sala"
-          />
+        <form onSubmit={handleSubmit}>
+          <ReactGA.OutboundLink
+            eventLabel="SEARCH_PAGE_INPUT"
+            to="/"
+            target="_self"
+          >
+            <input
+              type="text"
+              className="room-input"
+              value={room}
+              onChange={(event) => setRoom(event.target.value)}
+              required
+              placeholder="Nome da sala"
+            />
+          </ReactGA.OutboundLink>
+
           <p className="tip">Você pode criar salas como "nome1/nome2/nome3"</p>
 
           <button type="submit">Entrar</button>
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
+
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 import SearchPage from "./pages/SearchPage";
 import WritePage from "./pages/WritePage";
